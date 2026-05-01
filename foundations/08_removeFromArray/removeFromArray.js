57649d797bc3b17260e22ce7b964a3b437d87144
rjced <256202735+RJCed@users.noreply.github.com>
2026-05-01 23:17:11 +0800

    finished 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let arg = Array.from(arguments);
+    let toRemove = arg.slice(1);
+    let array = arg[0];
+
+    return array.filter(items => !toRemove.includes(items));
 };
 
 // Do not edit below this line
