8aa177eb6b114d135147399e3eb1fa4c419578ee
rjced <256202735+RJCed@users.noreply.github.com>
2026-05-01 22:22:19 +0800

    finished 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const word = "Hello World";
 
+const reverseString = function(str) {
+    //make a VARIABLE of the STRING
+    //SPLIT the STRING into letters on to an array
+    //REVERSE the array
+    //RETURN: JOIN the array 
+
+    let string = str;
+    let arr = string.split('');
+    arr.reverse()
+    
+    return arr.join('');
 };
+
+reverseString(word)
 
 // Do not edit below this line
 module.exports = reverseString;
