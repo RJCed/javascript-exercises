6c817ba7f9d87f9da27cfe89fd0a26e6cbe0ea93
rjced <256202735+RJCed@users.noreply.github.com>
2026-05-02 19:54:57 +0800

    finished 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+
+const addRange = function(min, max){ // Add the numbers from the minimum value to the maximum
+     let total = 0;
+     for (let i = min; i <= max; i++){
+        total = total + i;
+     }
+     return total;
+}
+
+const sumAll = function(num1, num2) { //Logic for each instance
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    if (num1 < num2 || num1 === num2){
+        return addRange(num1, num2);
+    } else if (num1 > num2){
+        return addRange(num2, num1)
+    }
 
 };
 
