d07ad54173b2f57fe79cbd2f63b12401213aed5c
rjced <256202735+RJCed@users.noreply.github.com>
2026-05-02 20:31:55 +0800

    finished 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
 
+// LEAP YEAR: Divisible by 4, Divisible by 400
+// NOT LEAP YEAR: Divisible by 100
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if ((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
