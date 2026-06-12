a3d28002b3b8d0b0368c0d101f2036baeef4913b
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-12 14:54:47 +0800

    finish 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const array = people;
+    const sorted = array.toSorted((a, b) => ((b.yearOfDeath === undefined ? new Date().getFullYear() : b.yearOfDeath) - b.yearOfBirth) - ((a.yearOfDeath === undefined ? new Date().getFullYear() : a.yearOfDeath)- a.yearOfBirth));
+    return sorted[0]
 };
 
 // Do not edit below this line
