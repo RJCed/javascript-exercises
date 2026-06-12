a8543644d810c71581dddb960e94cad1f5eaaefb
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-12 14:08:08 +0800

    finsihed 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0){
+        return "OOPS";
+    } else {
+        let fibo = [0 , 1];
 
+        for (let i = 1; i < index; i++){
+            fibo.push(fibo[i - 1] + fibo[i]);
+        }
+        
+        return fibo[index];
+    }
 };
 
 // Do not edit below this line
