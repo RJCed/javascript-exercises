0799c138cf70a2848115a47d17aa40e027e54524
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-12 12:59:03 +0800

    finished 12_CALCULATOR

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0){
+    return 0;
+  } else {
+    return numbers.reduce((a, b) => a + b);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if (numbers.length === 0){
+    return 0;
+  } else {
+    return numbers.reduce((a, b) => a * b);
+  }
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+	for (number; 0 < number; number--){
+    total = number * total;
+  }
+  return total;
 };
 
 // Do not edit below this line
