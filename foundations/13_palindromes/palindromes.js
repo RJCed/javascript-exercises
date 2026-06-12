70cf6c26fe59b7bca1142ac74c9a1eff64a68770
arjay <256202735+RJCed@users.noreply.github.com>
2026-06-12 13:44:45 +0800

    finished 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palindromes) {
+    let characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    const item = [...palindromes.toLowerCase()].filter(char => characters.includes(char)).join('');
+    const itemReverse = item.split('').toReversed().join('');
 
+
+    console.log(item);
+    console.log(itemReverse);
+    if (item === itemReverse){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
